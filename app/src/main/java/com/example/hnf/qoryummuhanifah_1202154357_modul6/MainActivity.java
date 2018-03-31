5e0998212a87987248fa62bc394a58d56fe46dc9
Qory Ummu Hanifah <jyhstalker@gmail.com>
2018-03-31 21:14:01 +0700

    commit #1

---
+++ app/src/main/java/com/example/hnf/qoryummuhanifah_1202154357_modul6/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hnf.qoryummuhanifah_1202154357_modul6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
